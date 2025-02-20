c1225398e88c4805b8947a8feee46edb7d416201
Emiliano Flores <jemilianofl@gmail.com>
2025-02-20 17:09:32 -0600

    Fix: Arreglo del menu de hamburguesa del Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,27 +14,25 @@ const Header = () => {
 
   return (
     <nav className="navbar-custom">
-      <div className="navbar-container">
-        {/* LOGO y MENÚ HAMBURGUESA */}
-        <div className="navbar-brand-container">
-          <Link className="navbar-brand" to="/"><img id="logo-header" src={LogoTWA} alt="Logo" /></Link>
-          <button className="navbar-toggle" onClick={toggleMenu}>☰</button>
-        </div>
 
-        {/* Contenedor de enlaces y botones */}
-        <div className="navbar-right">
-          <div className={`navbar-links ${menuActive ? "active" : ""}`}>
-            <Link className="nav-link" to="/">Inicio</Link>
-            <Link className="nav-link" to="/QuienesSomos">¿Quiénes somos?</Link>
-            <Link className="nav-link" to="/Cursos">Cursos/Talleres</Link>
-            <Link className="nav-link" to="/Contactanos">Contactanos</Link>
-          </div>
-          <div className={`navbar-buttons ${menuActive ? "active" : ""}`}>
-            <button className="btn btn-unirse"><Link className="nav-link" to="/Unirse">Unirse</Link></button>
-            <button className="btn btn-iniciar"><Link className="nav-link" to="/Iniciar">Iniciar</Link></button>
-          </div>
-        </div>
+      <div className="logo">
+        <img id="logo-header" src={LogoTWA} alt="Logo" />
       </div>
+      <div className="hamburguer">
+        <button className="navbar-toggle" onClick={toggleMenu}>☰</button>
+      </div>
+      <ul className={`navbar-links ${menuActive ? "active" : ""}`}>
+        <Link className="nav-link" to="/">Inicio</Link>
+        <Link className="nav-link" to="/QuienesSomos">¿Quiénes somos?</Link>
+        <Link className="nav-link" to="/Cursos">Cursos/Talleres</Link>
+        <Link className="nav-link" to="/Contactanos">Contactanos</Link>
+      </ul>
+
+      <div className={`navbar-buttons ${menuActive ? "active" : ""}`}>
+        <button className="btn btn-unirse"><Link className="nav-link" to="/Unirse">Unirse</Link></button>
+        <button className="btn btn-iniciar"><Link className="nav-link" to="/Iniciar">Iniciar</Link></button>
+      </div>
+
     </nav>
   );
 };
